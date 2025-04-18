a0c7235a99b3a00107272676a54d631ffae77d57
YissDev <houndetonamos@gmail.com>
2025-04-18 11:39:56 +0200

    ajoute de nouveau allowed

--- personLoan/settings.py
+++ personLoan/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-8asf_v@5g41t_^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['personLoan.onrender.com']
+ALLOWED_HOSTS = ['personloan-3.onrender.com']
 
 
 # Application definition
